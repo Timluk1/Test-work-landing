8ba13afe810f4f86f60683c9fa129b63b9d6dbd4
Timluk1 <main345242@gmail.com>
2025-01-24 15:23:27 +0300

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/Test-work-landing/",
     resolve: {
         alias: {
             assets: "/src/assets",
